9aabafc1e359525aedb658be34c1299d6aaeab3d
unknown <mixeew.roman2000@mail.ru>
2023-11-04 12:01:05 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const slides = Array.from(slider.querySelectorAll('img'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+var i = 1;
+  setInterval(function(){
+    showNextSlide();
+  i++;
+  if(i == 5)
+  {
+   i = 1;
+  }
+},2000)
